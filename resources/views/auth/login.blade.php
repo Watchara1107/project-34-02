6c4c1ae31eb90c786e0a1d0a31077bd5ecd5d9a6
Watchara Ketkaew <wk996110@gmail.com>
2023-08-21 10:47:13 +0700

    update Authentication

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,10 +21,7 @@
                     <div class="mb-3">
                       <input type="password" name="password" class="form-control form-control-lg" placeholder="Password" aria-label="Password">
                     </div>
-                    <div class="form-check form-switch">
-                      <input class="form-check-input" type="checkbox" id="rememberMe">
-                      <label class="form-check-label" for="rememberMe">Remember me</label>
-                    </div>
+                   
                     <div class="text-center">
                       <input type="submit" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0" value="Sign in">
                     </div>
@@ -33,7 +30,7 @@
                 <div class="card-footer text-center pt-0 px-lg-2 px-1">
                   <p class="mb-4 text-sm mx-auto">
                     Don't have an account?
-                    <a href="javascript:;" class="text-primary text-gradient font-weight-bold">Sign up</a>
+                    <a href="{{ route('register') }}" class="text-primary text-gradient font-weight-bold">Sign up</a>
                   </p>
                 </div>
               </div>
